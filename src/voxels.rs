00b027a269cbbc113be73cbc1cf4b86954ff09d9
Kai Patel <00kaipatel@gmail.com>
2023-05-25 17:13:18 +0100

    fix: vertex/indices generation for cube

--- src/voxels.rs
+++ src/voxels.rs
@@ -3,6 +3,42 @@ use crate::engine;
 
 const CHUNK_SIZE: usize = 16;
 
+const CUBE_VERTICES: [[f32; 3]; 24] = [
+    [-1.0, -1.0, 1.0],
+    [1.0, -1.0, 1.0],
+    [1.0, 1.0, 1.0],
+    [-1.0, 1.0, 1.0],
+    [-1.0, 1.0, -1.0],
+    [1.0, 1.0, -1.0],
+    [1.0, -1.0, -1.0],
+    [-1.0, -1.0, -1.0],
+    [1.0, -1.0, -1.0],
+    [1.0, 1.0, -1.0],
+    [1.0, 1.0, 1.0],
+    [1.0, -1.0, 1.0],
+    [-1.0, -1.0, 1.0],
+    [-1.0, 1.0, 1.0],
+    [-1.0, 1.0, -1.0],
+    [-1.0, -1.0, -1.0],
+    [1.0, 1.0, -1.0],
+    [-1.0, 1.0, -1.0],
+    [-1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0],
+    [1.0, -1.0, 1.0],
+    [-1.0, -1.0, 1.0],
+    [-1.0, -1.0, -1.0],
+    [1.0, -1.0, -1.0],
+];
+
+const CUBE_INDICES: &[u16] = &[
+    0, 1, 2, 2, 3, 0, // top
+    4, 5, 6, 6, 7, 4, // bottom
+    8, 9, 10, 10, 11, 8, // right
+    12, 13, 14, 14, 15, 12, // left
+    16, 17, 18, 18, 19, 16, // front
+    20, 21, 22, 22, 23, 20, // back
+];
+
 #[derive(Copy, Clone, Default, Debug)]
 enum BlockType {
     #[default]
@@ -15,17 +51,18 @@ struct Chunk {
     blk: [[[BlockType; CHUNK_SIZE]; CHUNK_SIZE]; CHUNK_SIZE],
     elements: usize,
     changed: bool,
-    vbo: wgpu::Buffer,
+    vertices: [engine::Vertex; CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE * 24],
+    indices: [u16; CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE * 36],
 }
 
 impl Default for Chunk {
     fn default() -> Self {
-        let vbo = engine::gen_buffers(1).remove(0);
         Self {
             blk: Default::default(),
             elements: 0,
             changed: false,
-            vbo,
+            vertices: [engine::Vertex::default(); CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE * 24],
+            indices: [0u16; CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE * 36],
         }
     }
 }
@@ -49,58 +86,25 @@ impl Chunk {
 
         let mut i = 0;
 
-        let mut vertices = &mut [engine::Vertex {
-            position: [0.0, 0.0, 0.0],
-            color: [0.0, 0.0, 0.0],
-        }; CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE * 6 * 6];
-
-        for x in 0..CHUNK_SIZE as u8 {
-            for y in 0..CHUNK_SIZE as u8 {
-                for z in 0..CHUNK_SIZE as u8 {
+        for x in 0..CHUNK_SIZE {
+            for y in 0..CHUNK_SIZE {
+                for z in 0..CHUNK_SIZE {
                     let _block_type = self.blk[x as usize][y as usize][z as usize];
-                    let vs = vec![
-                        [x, y, z],
-                        [x, y, z + 1],
-                        [x, y + 1, z],
-                        [x, y + 1, z],
-                        [x, y, z + 1],
-                        [x, y + 1, z + 1],
-                        [x + 1, y, z],
-                        [x + 1, y + 1, z],
-                        [x + 1, y, z + 1],
-                        [x + 1, y + 1, z],
-                        [x + 1, y + 1, z + 1],
-                        [x + 1, y, z + 1],
-                        [x, y, z],
-                        [x + 1, y, z],
-                        [x, y, z + 1],
-                        [x + 1, y, z],
-                        [x + 1, y, z + 1],
-                        [x, y, z + 1],
-                        [x, y + 1, z],
-                        [x, y + 1, z + 1],
-                        [x + 1, y + 1, z],
-                        [x + 1, y + 1, z],
-                        [x, y + 1, z + 1],
-                        [x + 1, y + 1, z + 1],
-                        [x, y, z],
-                        [x, y + 1, z],
-                        [x + 1, y, z],
-                        [x, y + 1, z],
-                        [x + 1, y + 1, z],
-                        [x + 1, y, z],
-                        [x, y, z + 1],
-                        [x + 1, y, z + 1],
-                        [x, y + 1, z + 1],
-                        [x, y + 1, z + 1],
-                        [x + 1, y, z + 1],
-                        [x + 1, y + 1, z + 1],
-                    ];
-                    for _ in 0..36 {
-                        vertices[i].position[0] = vs[i][0] as f32;
-                        vertices[i].position[1] = vs[i][1] as f32;
-                        vertices[i].position[2] = vs[i][2] as f32;
-                        vertices[i].color = [1.0, 0.0, 0.0];
+
+                    let xf = x as f32;
+                    let yf = y as f32;
+                    let zf = z as f32;
+
+                    for idx in 0..CUBE_INDICES.len() {
+                        self.indices[idx] =
+                            CUBE_INDICES[idx] + (x + CHUNK_SIZE * (y + CHUNK_SIZE * z)) as u16;
+                    }
+
+                    for v in CUBE_VERTICES {
+                        self.vertices[i].position[0] = xf + v[0];
+                        self.vertices[i].position[1] = yf + v[1];
+                        self.vertices[i].position[2] = zf + v[2];
+                        self.vertices[i].color = [1.0, 0.0, 0.0];
                         i += 1;
                     }
                 }
@@ -108,8 +112,6 @@ impl Chunk {
         }
 
         self.elements = i;
-        let vbo = engine::create_buffer(vertices);
-        self.vbo = vbo;
     }
 
     pub fn render(&mut self) {
