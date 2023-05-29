4b3b2be2f1a30ab73c7f412c69719b4d3eb1cfa5
Kai Patel <00kaipatel@gmail.com>
2023-05-29 15:15:08 +0100

    chore: remove dead VERTICES, INDICES

--- src/engine.rs
+++ src/engine.rs
@@ -38,23 +38,6 @@ impl Vertex {
     }
 }
 
-const VERTICES: &[Vertex] = &[
-    Vertex {
-        position: [0.0, 0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
-    },
-    Vertex {
-        position: [-0.5, -0.5, 0.0],
-        color: [0.0, 1.0, 0.0],
-    },
-    Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [0.0, 0.0, 1.0],
-    },
-];
-
-const INDICES: &[u16] = &[0, 1, 2];
-
 struct Camera {
     eye: cgmath::Point3<f32>,
     target: cgmath::Point3<f32>,
